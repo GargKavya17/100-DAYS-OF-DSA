e58267eac91c56ba834388104797177db25fe52e
Kavya Garg <Gargkavya931@gmail.com>
2026-02-05 22:16:15 +0530

    DONE

---
+++ D5Q1.c
@@ -0,0 +1,47 @@
+// Merged Array in ascending order
+#include <stdio.h>
+int main() {
+    int n1, n2;
+    printf("Enter the size of first array: ");
+    scanf("%d", &n1);
+    int arr1[n1];
+    printf("Enter the elements of first array: ");
+    for (int i = 0; i < n1; i++) {
+        scanf("%d", &arr1[i]);
+    }
+    
+    printf("Enter the size of second array: ");
+    scanf("%d", &n2);
+    int arr2[n2];
+    printf("Enter the elements of second array: ");
+    for (int i = 0; i < n2; i++) {
+        scanf("%d", &arr2[i]);
+    }
+    
+    int merged[n1 + n2];
+    int i = 0, j = 0, k = 0;
+    
+    while (i < n1 && j < n2) {
+        if (arr1[i] < arr2[j]) {
+            merged[k++] = arr1[i++];
+        } else {
+            
+            merged[k++] = arr2[j++];
+        }
+    }
+    
+    while (i < n1) {
+        merged[k++] = arr1[i++];
+    }
+    
+    while (j < n2) {
+        merged[k++] = arr2[j++];
+    }
+    
+    printf("Merged array in ascending order: ");
+    for (int i = 0; i < k; i++) {
+        printf("%d ", merged[i]);
+    }
+    
+    return 0;
+}
