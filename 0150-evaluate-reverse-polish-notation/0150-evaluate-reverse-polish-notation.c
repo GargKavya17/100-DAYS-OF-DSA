76228de4fdaf8b8aaf74dc233148a17af9319cd1
Kavya Garg <Gargkavya931@gmail.com>
2026-03-08 11:57:14 +0530

    Time: 0 ms (100%), Space: 10.4 MB (22.01%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.c
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.c
@@ -1,30 +1,30 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 int evalRPN(char ** tokens, int tokensSize) {
-    int* stack = (int*)malloc(sizeof(int) * tokensSize);
+    int* stack = (int*)malloc(tokensSize * sizeof(int));
     int top = -1;
 
     for (int i = 0; i < tokensSize; i++) {
         char* s = tokens[i];
 
-        if ((s[0] == '+' || s[0] == '-' || s[0] == '*' || s[0] == '/') && strlen(s) == 1) {
-            int val2 = stack[top--]; 
-            int val1 = stack[top--]; 
+        if ((strlen(s) == 1) && (s[0] == '+' || s[0] == '-' || s[0] == '*' || s[0] == '/')) {
+            int b = stack[top--];
+            int a = stack[top--]; 
             
             switch (s[0]) {
-                case '+': stack[++top] = val1 + val2; break;
-                case '-': stack[++top] = val1 - val2; break;
-                case '*': stack[++top] = val1 * val2; break;
-                case '/': stack[++top] = val1 / val2; break;
+                case '+': stack[++top] = a + b; break;
+                case '-': stack[++top] = a - b; break;
+                case '*': stack[++top] = a * b; break;
+                case '/': stack[++top] = a / b; break; 
             }
         } else {
             stack[++top] = atoi(s);
         }
     }
 
-    int result = stack[0];
+    int result = stack[top];
     free(stack);
     return result;
 }
