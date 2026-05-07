01b3236ab247d0d999d20a09705729698259afbf
Kavya Garg <Gargkavya931@gmail.com>
2026-05-07 11:40:14 +0530

    DONE

---
+++ D93Q1.c
@@ -0,0 +1,31 @@
+// Problem: Sort an array using insertion sort.
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int data[] = {12, 11, 13, 5, 6};
+    int n = sizeof(data) / sizeof(data[0]);
+    
+    insertionSort(data, n);
+    printArray(data, n);
+    
+    return 0;
+}
