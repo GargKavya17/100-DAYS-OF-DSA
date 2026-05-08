2b10f220af21f115c5f2709f1daae08dfac39305
Kavya Garg <Gargkavya931@gmail.com>
2026-05-08 16:47:29 +0530

    DONE

---
+++ D94Q1.c
@@ -0,0 +1,36 @@
+// Problem: Sort array of non-negative integers using counting sort. Find max, build freq array, compute prefix sums, build output.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void countingSort(int* arr, int n) {
+    if (n <= 1) return;
+
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) max = arr[i];
+    }
+
+    int* count = (int*)calloc(max + 1, sizeof(int));
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    for (int i = 1; i <= max; i++) {
+        count[i] += count[i - 1];
+    }
+
+    int* output = (int*)malloc(n * sizeof(int));
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+
+    free(count);
+    free(output);
+}
